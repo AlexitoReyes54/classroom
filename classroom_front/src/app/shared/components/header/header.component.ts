28685b6b90e22368d666a480e910789eb660a794
Alexander Reyes <alexandereliasr9@gmail.com>
2022-01-07 06:00:41 -0400

    header changes

--- classroom_front/src/app/shared/components/header/header.component.ts
+++ classroom_front/src/app/shared/components/header/header.component.ts
@@ -11,27 +11,8 @@ export class HeaderComponent implements OnInit {
 
   constructor(private router:Router,private credentialStorageSvc:CredentialStorageService,) { }
 
-  showAdminManagement:boolean = false;
-  showHome:boolean = false;
-  showSignOut:boolean = false;
 
   ngOnInit(): void {
-    this.showOptions()
-  }
-  ngOnChanges(simple:SimpleChanges){
-
-  }
-  
-  showOptions(){
-    let currentRole = this.credentialStorageSvc.getCredentials().roleId
-
-    if (currentRole == 2) {
-      this.showAdminManagement = true;
-      this.showSignOut = true;
-    }else if(currentRole == 1 || currentRole == 2){
-      this.showHome = true;
-      this.showSignOut = true;
-    }
   }
 
   singOut(){
